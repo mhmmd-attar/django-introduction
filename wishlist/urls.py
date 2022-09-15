a3c3a421d29cab1ee4eeb6d85f75bb05ad45e431
mhmmd-attar <mohammad.attar.w@gmail.com>
2022-09-15 10:28:34 +0700

    Update views.py to show data in xml by id

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id
+from wishlist.views import * 
 
 app_name = 'wishlist'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'), 
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -19,6 +19,10 @@ def show_json(request):
     data =ItemWishlist.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def show_xml_by_id(request, id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
 def show_json_by_id(request, id):
     data = ItemWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
